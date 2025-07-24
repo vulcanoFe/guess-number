3d294b8939ad11b92bf041c7554f4e4482d8f573
Federico <vulcanofederico94@gmail.com>
2025-07-24 15:18:16 +0200

    landscape view

--- app/screens/GameOverScreen.tsx
+++ app/screens/GameOverScreen.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 import GameHistory, { GameResult } from "../components/game/GameHistory";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -11,21 +11,26 @@ interface GameOverScreenProps {
 }
 
 function GameOverScreen({ onStartNewGame, gameHistory }: GameOverScreenProps) {
+
+	const { width } = useWindowDimensions();
+
 	return (
 		<View style={style.gameScreen}>
 			<Title>End of the game!</Title>
-			<View style={style.resultContainer}>
-				{
-					gameHistory && gameHistory.length > 0 &&
-					(<NumberContainer>{gameHistory[gameHistory.length - 1]?.guessedNumber}</NumberContainer>)
-				}
-				<View style={style.buttonContainer}>
-					<PrimaryButton onPressHandler={onStartNewGame}>
-						<Ionicons name="refresh" size={24} color="white" />
-					</PrimaryButton>
+			<View style={width < 500 ? style.verticalContainer : style.horizontalContainer}>
+				<View style={style.resultContainer}>
+					{
+						gameHistory && gameHistory.length > 0 &&
+						(<NumberContainer>{gameHistory[gameHistory.length - 1]?.guessedNumber}</NumberContainer>)
+					}
+					<View style={style.buttonContainer}>
+						<PrimaryButton onPressHandler={onStartNewGame}>
+							<Ionicons name="refresh" size={24} color="white" />
+						</PrimaryButton>
+					</View>
 				</View>
+				<GameHistory gameHistory={gameHistory} />
 			</View>
-			<GameHistory gameHistory={gameHistory} />
 		</View>
 	)
 }
@@ -36,6 +41,17 @@ const style = StyleSheet.create({
 		padding: 24,
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	verticalContainer: {
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	horizontalContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		width: '100%',
 	},
 	resultContainer: {
 		flexDirection: 'row',
